23d0e591021fd4eec8e3770a113b92409f2a5d45
David Grimm <saempfglas@googlemail.com>
2025-03-23 19:12:32 +0100

    started

--- backend/studious_spork/models.py
+++ backend/studious_spork/models.py
@@ -2,6 +2,7 @@ from bson import ObjectId
 from pydantic import Field, BaseModel, AfterValidator, PlainSerializer, WithJsonSchema
 from typing import Optional, Any, Annotated, Union
 
+from pydantic.json_schema import JsonSchemaValue
 from pydantic_core import core_schema
 
 
@@ -11,21 +12,39 @@ from pydantic.functional_validators import AfterValidator
 from bson import ObjectId as _ObjectId
 
 
-def check_object_id(value: str) -> str:
-    if not _ObjectId.is_valid(value):
-        raise ValueError('Invalid ObjectId')
-    return value
+class ObjectIdPydanticAnnotation:
+    @classmethod
+    def validate_object_id(cls, v: Any, handler) -> ObjectId:
+        if isinstance(v, ObjectId):
+            return v
+
+        s = handler(v)
+        if ObjectId.is_valid(s):
+            return ObjectId(s)
+        else:
+            raise ValueError("Invalid ObjectId")
+
+    @classmethod
+    def __get_pydantic_core_schema__(cls, source_type, _handler) -> core_schema.CoreSchema:
+        assert source_type is ObjectId
+        return core_schema.no_info_wrap_validator_function(
+            cls.validate_object_id,
+            core_schema.str_schema(),
+            serialization=core_schema.to_string_ser_schema(),
+        )
+
+    @classmethod
+    def __get_pydantic_json_schema__(cls, _core_schema, handler) -> JsonSchemaValue:
+        return handler(core_schema.str_schema())
 
 
-ObjectId = Annotated[str, AfterValidator(check_object_id)]
 
 
-class MongoBaseModel(BaseModel):
-    id: ObjectId = Field(default_factory=ObjectId, alias="_id")
+class Model(BaseModel):
+    id: Annotated[ObjectId, ObjectIdPydanticAnnotation]
 
 
-
-class CarBase(MongoBaseModel):
+class CarBase(BaseModel):
     brand: str = Field(..., min_length=3)
     make: str = Field(..., min_length=3)
     year: int = Field(..., gt=1975, lt=2023)
@@ -34,7 +53,7 @@ class CarBase(MongoBaseModel):
     cm3: int = Field(...)
 
 
-class CarUpdate(MongoBaseModel):
+class CarUpdate(BaseModel):
     price: Optional[int] = None
 
 
